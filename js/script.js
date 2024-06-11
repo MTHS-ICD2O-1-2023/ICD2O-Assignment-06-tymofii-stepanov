a95e8d015c2c41460f12dcb293dae8f8ae4efb2c
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-06-11 00:13:16 +0000

    done with code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Coxal All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Tymofii
+// Created on: Jun 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  document.getElementById("new-quote").addEventListener("click", fetchQuote)
+  fetchQuote()
+}
+
+function fetchQuote() {
+  const url = "https://zenquotes.io/api/random"
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data && Array.isArray(data) && data.length > 0) {
+        const quote = data[0].q
+        const author = data[0].a
+        document.getElementById("quote").innerHTML = `"${quote}" - ${author}`
+      } else {
+        document.getElementById("quote").innerHTML = "No quote available, please try again."
+      }
+    })
+    .catch(error => {
+      console.error("Error fetching the quote: ", error)
+      document.getElementById("quote").innerHTML = "Failed to fetch quote, please try again."
+    })
+}
